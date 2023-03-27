6081f536fce66830592e54a15fff41cd4a0a07ca
Awais Rashid <awais2075@Awais-Rashid.local>
2023-03-27 12:53:59 +0500

    initial commit

---
+++ src/test/java/com/countries/next/rest/RestNextCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countries.next.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNextCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
