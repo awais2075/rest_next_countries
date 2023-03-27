9fa2987a24e2e1ced19b79a41e34c9a70c2328ee
Awais Rashid <awais2075@Awais-Rashid.local>
2023-03-27 13:16:27 +0500

    added mongodb-config

---
+++ src/main/java/com/countries/next/rest/config/MongoConfig.java
@@ -0,0 +1,34 @@
+package com.countries.next.rest.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${mongodb.connection-string}")
+    private String url;
+    @Value("${mongodb.database}")
+    private String database;
+
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(url);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), database);
+    }
+}
